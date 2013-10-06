caf910ea732eaf799ca2fb723031643f2f394e79
testmrpaxton <mr_paxton@hotmail.com>
2013-10-06 16:40:43 -0700

    Validations!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
-  #fixtures :products
+  fixtures :products
 
   test "product attributes must not be empty" do
     product = Product.new
