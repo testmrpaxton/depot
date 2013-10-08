b3b30961f020960d41798333090613ce0826dec8
testmrpaxton <mr_paxton@hotmail.com>
2013-10-07 18:35:55 -0700

    add store controller

--- rails_ready.rb
+++ rails_ready.rb
@@ -5,12 +5,29 @@ def open_new_tab( folder )
 	%x[ osascript 2>/dev/null <<EOF
 		     tell application "Terminal"
 		     tell application "System Events" to keystroke "t" using command down
-		     do script with command "cd #{folder}; clear; ls -pRF" in front window
+		     do script with command "cd '#{folder}'; clear" in front window
 		     end tell
 		     EOF
 	]
 end
 
-//folder = Dir.getwd + "app/" + ARGV[0]
+folder = Dir.getwd + "/app/" + ARGV[0]
+
+#views
+#controllers
+#models
+#config
+#test
+#log
+#db
+#layouts
+
+
+puts folder
 
 open_new_tab( folder )
+
+
+#echo -ne '\033]0;#{folder}\007'
+
+
